3ceadee5e6ab7dbb1ebf02c6cd03aaef48163b73
riste238 <60303629+riste238@users.noreply.github.com>
2022-01-18 16:58:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+// array for create random sentence
 let allText = [
  "Ova e prv text",
  "Ova e vtor text",
@@ -7,21 +8,20 @@ let allText = [
 
 let startBtn = document.querySelector('#startBtn');
 let mainText = document.querySelector('#main-text');
+
+//textarea
 let  comp1Area = document.querySelector('#comp1-area');
 let  comp2Area = document.querySelector('#comp2-area');
 let  comp3Area = document.querySelector('#comp3-area');
 let  playerArea = document.querySelector('#player-area');
 
-
+// buttons
 let c1Btn = document.querySelector('#c1Btn');
 let c2Btn = document.querySelector('#c2Btn');
 let c3Btn = document.querySelector('#c3Btn');
 let playerBtn = document.querySelector('#playerBtn');
 
-
 let position = 0;
-
-
 
 startBtn.addEventListener('click',startTimer);
 
@@ -54,8 +54,7 @@ function startTimer() {
   }
 
   function getRandomText(){
-    mainText.innerHTML = allText[Math.floor(Math.random()* allText.length)]; // a zosto ne raboti kako vakva?
-    // ista cel kje dobieme ako ja staveme ovaa funkcionalnost i vo edna varijabla, 
+    mainText.innerHTML = allText[Math.floor(Math.random()* allText.length)]; 
     let rand = Math.floor(Math.random()* allText.length);
      return allText[rand];
   }
@@ -69,12 +68,9 @@ function startTimer() {
            position++;
            playerBtn.innerHTML = "Position "+ position;
            playerBtn.className = "btn btn-success form-control";
-       
-
          }
          else {
            //wrong, 
-          //  ako ne e tocno vnesen dadeniot text, ti napravi nesto drugo ovde vo player textarea.
           playerBtn.innerHTML =  "WRONG TYPING";
           playerBtn.className = "btn btn-danger form-control";
          }
@@ -103,57 +99,41 @@ function startTimer() {
          c1Btn.className = "btn btn-success form-control";
 
        }
-       }, 400);
+       },400);
 
     }
 
     function comp2Typing(){
       let counter = 0;
       let textForTyping =  mainText.innerHTML;
-        
-  
       let textForTypingArr = textForTyping.split("");
-
-       let loop = setInterval(() => {
-         
-       if(textForTypingArr.length > 0){
-     
+      let loop = setInterval(() => {
+      if(textForTypingArr.length > 0){
       comp2Area.value += textForTypingArr.shift();
-                 
-       }
+      }
        else{
          clearInterval(loop);
          position++;
         c2Btn.innerHTML = "Position: " + position;
         c2Btn.className = "btn btn-success form-control";
-
        }
        }, 300);
-
     }
-
 
     function comp3Typing(){
       let counter = 0;
       let textForTyping =  mainText.innerHTML;
-        
-  
       let textForTypingArr = textForTyping.split("");
-
-       let loop = setInterval(() => {
-         
-       if(textForTypingArr.length > 0){
-     
+      let loop = setInterval(() => {
+      if(textForTypingArr.length > 0){
       comp3Area.value += textForTypingArr.shift();
-                 
        }
        else{
          clearInterval(loop);
          position++;
          c3Btn.innerHTML = "Position: " + position;
          c3Btn.className = "btn btn-success form-control";
-
-       }
+          }
        }, 270);
 
     }
